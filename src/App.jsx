2ca7476b6079521360de01dac7b4c8d7e35d5a78
Benjamin Cortes <benjamca@hotmail.com>
2023-07-05 23:14:51 -0400

    Creacion del modal para agregar gastos

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
+import { useId, useState } from 'react'
 import { Header } from './components/Header'
+import { generarId } from './helpers/index'
 import IconoNuevoGasto from './img/nuevo-gasto.svg';
 import { Modal } from './components/Modal';
 function App() {
@@ -7,8 +8,29 @@ function App() {
  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
 
 const [modal, setModal] = useState(false)
+const [animarModal, setAnimarModal] = useState(false)
+
+const [gastos, setGastos] = useState([])
+
  const handleNuevoGasto = () => {
   setModal(true)
+  
+  setTimeout(() => {
+    setAnimarModal(true)
+  },500)
+ }
+
+ const guardarGasto = gasto => {
+  gasto.id = generarId()
+  setGastos([...gastos, gasto])
+
+  setAnimarModal(false)
+
+  setTimeout(() => {
+    setModal(false)
+  },500)
+
+  
  }
 
 
@@ -33,6 +55,9 @@ const [modal, setModal] = useState(false)
     {modal && 
     <Modal 
     setModal={setModal}
+    animarModal={animarModal}
+    setAnimarModal={setAnimarModal}
+    guardarGasto={guardarGasto}
     />}
    
     </>
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,38 @@
+import { useState } from 'react';
 import IconoCerrarBtn from '../img/cerrar.svg';
+import { Mensaje } from './Mensaje';
 
-export const Modal = ({ setModal }) => {
-    const ocultarModal = () => {
-        setModal(false)
+export const Modal = ({ setModal, animarModal, setAnimarModal, guardarGasto }) => {
+   
+  const [nombre, setNombre] = useState('')
+  const [cantidad, setCantidad] = useState('')
+  const [categoria, setCategoria] = useState('')
+  const [mensaje, setMensaje] = useState('')
+
+  const ocultarModal = () => {
+        
+        setAnimarModal(false)
+
+        setTimeout(() => {
+          setModal(false)
+        },500)
     }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if ([nombre, cantidad, categoria].includes('')) {
+      // verifica los campos que no esten vacios
+      setMensaje('Todos los campos son obligatorios')
+
+      setTimeout(() => {
+        setMensaje('')
+      },3000)
+      return
+    }
+
+    guardarGasto({nombre, cantidad, categoria})
+  }  
 
   return (
     <div className='modal'>
@@ -14,6 +43,62 @@ export const Modal = ({ setModal }) => {
         onClick={ocultarModal}
         />
         </div>
+        <form 
+        className={`formulario ${animarModal ? "animar" : 'cerrar'}`}
+        onSubmit={handleSubmit}
+        >
+
+          <legend>Nuevo Gasto</legend>
+
+          {mensaje && <Mensaje tipo='error'>{mensaje}</Mensaje>}
+
+          <div className='campo'>
+            <label htmlFor='nombre'>Nombre Gasto</label>
+            <input 
+            id='nombre'
+            type='text'
+            placeholder='Añade el nombre del gasto'
+            value={nombre}
+            onChange={e => setNombre(e.target.value)}
+            />
+          </div>
+
+          <div className='campo'>
+            <label htmlFor='cantidad'>Cantidad</label>
+            <input 
+            id='cantidad'
+            type='number'
+            placeholder='Añade la cantidad del gasto'
+            value={cantidad}
+            onChange={e => setCantidad(Number(e.target.value))}
+            />
+          </div>
+
+          <div className='campo'>
+            <label htmlFor='categoria'>Categoria</label>
+           <select 
+           id='categoria'
+           value={categoria}
+           onChange={e => setCategoria(e.target.value)}
+           >
+            <option value=''>-- Seleccione --</option>
+            <option value='ahorro'>Ahorro</option>
+            <option value='comida'>Comida</option>
+            <option value='casa'>Casa</option>
+            <option value='salud'>Salud</option>
+            <option value='ocio'>Ocio</option>
+            <option value='gastos'>Gastos Varios</option>
+            <option value='subcripciones'>Subcripciones</option>
+           </select>
+          </div>
+
+          <input 
+          type='submit'
+          value='Añadir Gasto'
+          />
+
+
+        </form>
     </div>
   )
 }
